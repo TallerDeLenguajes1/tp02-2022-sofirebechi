a79014a189f88dd7331177f0a5de457a374014d9
Sofia <sofia.rebechi@gmail.com>
2022-03-31 19:56:37 -0300

    septimo commit, punto 4

---
+++ tp2_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct compu {
+int velocidad;//entre 1 y 3 Gherz
+int anio;//entre 2000 y 2017
+int cantidad;//entre 1 y 4
+char *tipo_cpu; //valores del arreglo tipos
+};
+
+int main(){
+
+    
+}
