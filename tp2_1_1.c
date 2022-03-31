fee068cfb39336f000530d55f63d2132d2cb9863
TRIXServer <admin@trixserver.com>
2022-03-31 18:22:12 -0300

    segundo commit, 1a hecho

---
+++ tp2_1_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 20
+
+int main(){
+
+    int i;
+    double vt[N];
+
+    for(i = 0;i<N; i++){
+           
+            vt[i] = 1 + rand() % 100; 
+            printf(" %f \n ", vt[i]);
+    }
+
+    return 0;
+}
+
